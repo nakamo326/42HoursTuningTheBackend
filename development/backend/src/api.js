3269ca442c3c53c622b08271281cf8ae10e38c32
Yasufumi Nakamoto <nakamo326@gmail.com>
2022-04-16 02:10:42 +0900

    fix getItem()

--- development/backend/src/api.js
+++ development/backend/src/api.js
@@ -32,13 +32,10 @@ const getItems = async (user, recordResult) => {
   console.log(recordResult.length);
   const items = Array(recordResult.length);
 
-  // 'select name from user where user_id = ?';
-  const searchUserQs = 'select * from user where user_id = ?';
-  // 'select name from group_info where group_id = ?';
-  const searchGroupQs = 'select * from group_info where group_id = ?';
-  // 'select item_id from record_item_file where linked_record_id = ? order by item_id asc limit 1';
+  const searchUserQs = 'select name from user where user_id = ?';
+  const searchGroupQs = 'select name from group_info where group_id = ?';
   const searchThumbQs =
-    'select * from record_item_file where linked_record_id = ? order by item_id asc limit 1';
+    'select item_id from record_item_file where linked_record_id = ? order by item_id asc limit 1';
   const countQs =
     'select count(*) from record_comment where linked_record_id = ?';
   const searchLastQs =
@@ -62,60 +59,47 @@ const getItems = async (user, recordResult) => {
 
       const line = record;
       // recordResultで必要なカラムはこれだけ？
-      const recordId = line.record_id;
-      const createdBy = line.created_by;
-      const applicationGroup = line.application_group;
-      const updatedAt = line.updated_at;
+      resObj.recordId = line.record_id;
+      resObj.title = line.title;
+      resObj.createdBy = line.created_by;
+      resObj.createAt = line.created_at;
+      resObj.applicationGroup = line.application_group;
+      resObj.updatedAt = line.updated_at;
       //
-      let createdByName = null;
-      let applicationGroupName = null;
-      let thumbNailItemId = null;
-      let commentCount = 0;
-      let isUnConfirmed = true;
 
-      const [userResult] = await pool.query(searchUserQs, [createdBy]);
+      const [userResult] = await pool.query(searchUserQs, [resObj.createdBy]);
       if (userResult.length === 1) {
-        createdByName = userResult[0].name;
+        resObj.createdByName = userResult[0].name;
       }
 
-      const [groupResult] = await pool.query(searchGroupQs, [applicationGroup]);
+      const [groupResult] = await pool.query(searchGroupQs, [
+        resObj.applicationGroup,
+      ]);
       if (groupResult.length === 1) {
-        applicationGroupName = groupResult[0].name;
+        resObj.applicationGroupName = groupResult[0].name;
       }
 
-      const [itemResult] = await pool.query(searchThumbQs, [recordId]);
+      const [itemResult] = await pool.query(searchThumbQs, [resObj.recordId]);
       if (itemResult.length === 1) {
-        thumbNailItemId = itemResult[0].item_id;
+        resObj.thumbNailItemId = itemResult[0].item_id;
       }
 
-      const [countResult] = await pool.query(countQs, [recordId]);
+      const [countResult] = await pool.query(countQs, [resObj.recordId]);
       if (countResult.length === 1) {
-        commentCount = countResult[0]['count(*)'];
+        resObj.commentCount = countResult[0]['count(*)'];
       }
 
       const [lastResult] = await pool.query(searchLastQs, [
         user.user_id,
-        recordId,
+        resObj.recordId,
       ]);
       if (lastResult.length === 1) {
-        const updatedAtNum = Date.parse(updatedAt);
+        const updatedAtNum = Date.parse(resObj.updatedAt);
         const accessTimeNum = Date.parse(lastResult[0].access_time);
         if (updatedAtNum <= accessTimeNum) {
-          isUnConfirmed = false;
+          resObj.isUnConfirmed = false;
         }
       }
-
-      resObj.recordId = recordId;
-      resObj.title = line.title;
-      resObj.applicationGroup = applicationGroup;
-      resObj.applicationGroupName = applicationGroupName;
-      resObj.createdBy = createdBy;
-      resObj.createdByName = createdByName;
-      resObj.createAt = line.created_at;
-      resObj.commentCount = commentCount;
-      resObj.isUnConfirmed = isUnConfirmed;
-      resObj.thumbNailItemId = thumbNailItemId;
-      resObj.updatedAt = updatedAt;
 
       items[i] = resObj;
     })
@@ -420,11 +404,11 @@ const allActive = async (req, res) => {
   mylog(recordResult);
   let count = 0;
 
-  // TODO:
   const items = await getItems(user, recordResult);
 
   const recordCountQs = 'select count(*) from record where status = "open"';
 
+  // TODO: 先に実行
   const [recordCountResult] = await pool.query(recordCountQs);
   if (recordCountResult.length === 1) {
     count = recordCountResult[0]['count(*)'];
