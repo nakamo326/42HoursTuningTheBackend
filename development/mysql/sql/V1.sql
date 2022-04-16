84ea98cb75690240cca1ae2f8796f92eb1ba6cc0
Yasufumi Nakamoto <nakamo326@gmail.com>
2022-04-16 17:37:04 +0900

    add index

--- development/mysql/sql/V1.sql
+++ development/mysql/sql/V1.sql
@@ -1,7 +1,7 @@
 -- sqlのindexを張る作業をする予定です。
 -- record
--- ALTER TABLE `record` ADD INDEX idx1(`created_by`, `status`);
-ALTER TABLE `record` ADD INDEX idx1(`created_by`, `status`, `updated_at` desc, `record_id` asc);
+ALTER TABLE `record` ADD INDEX idx1(`status`, `updated_at` desc, `record_id` asc);
+ALTER TABLE `record` ADD INDEX idx2(`created_by`, `status`, `updated_at` desc, `record_id` asc);
 
 -- record_item_file
 ALTER TABLE `record_item_file` ADD INDEX idx1(`linked_record_id`, `item_id` asc);
