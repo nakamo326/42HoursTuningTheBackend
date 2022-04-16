38b8e6b0575762ec60befe460215aa36ba7b9ddd
mahiro <mahiro.yoshie@gmail.com>
2022-04-17 04:17:53 +0900

    index変更

--- development/mysql/sql/V1.sql
+++ development/mysql/sql/V1.sql
@@ -1,7 +1,11 @@
 -- sqlのindexを張る作業をする予定です。
 -- record
-ALTER TABLE `record` ADD INDEX idx1(`status`, `updated_at` desc, `record_id` asc);
-ALTER TABLE `record` ADD INDEX idx2(`created_by`, `status`, `updated_at` desc, `record_id` asc);
+
+-- add
+ALTER TABLE `record` ADD INDEX idx1(`category_id`, `application_group`);
+
+ALTER TABLE `record` ADD INDEX idx2(`status`, `updated_at` desc, `record_id` asc);
+ALTER TABLE `record` ADD INDEX idx3(`created_by`, `status`, `updated_at` desc, `record_id` asc);
 
 -- record_item_file
 ALTER TABLE `record_item_file` ADD INDEX idx1(`linked_record_id`, `item_id` asc);
--- development/mysql/sql/V2.sql
+++ development/mysql/sql/V2.sql
@@ -1,4 +1,8 @@
 
 ALTER TABLE `group_member` ADD INDEX idx1(`user_id`, `is_primary`);
 
-ALTER TABLE `record` ADD INDEX idx3(`status`);
+-- add
+ALTER TABLE `group_member` ADD INDEX idx2(`group_id`);
+
+-- add
+ALTER TABLE `category_group` ADD INDEX idx1(`group_id`);
