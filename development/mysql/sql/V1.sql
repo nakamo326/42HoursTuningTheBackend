fed245a46dec522e8bd800565f3a36c39130e80c
Yasufumi Nakamoto <nakamo326@gmail.com>
2022-04-16 13:35:55 +0900

    index追加

--- development/mysql/sql/V1.sql
+++ development/mysql/sql/V1.sql
@@ -1,1 +1,7 @@
 -- sqlのindexを張る作業をする予定です。
+-- record
+ALTER TABLE record ADD INDEX idx1(status, updated_at desc, record_id asc);
+-- record_item_file
+ALTER TABLE record_item_file ADD INDEX idx1(linked_record_id, item_id asc);
+-- session 
+ALTER TABLE session ADD INDEX idx1(value);
