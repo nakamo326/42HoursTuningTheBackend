eb2aed262c8f051ede6185d36ae8787e1ccb4726
mahiro <mahiro.yoshie@gmail.com>
2022-04-17 05:22:15 +0900

    index(record)  status追加

--- development/mysql/sql/V2.sql
+++ development/mysql/sql/V2.sql
@@ -1,8 +1,7 @@
 ALTER TABLE `group_member` ADD INDEX idx1(`user_id`, `is_primary`);
 
--- add
+-- group_member
 ALTER TABLE `group_member` ADD INDEX idx2(`group_id`);
 
--- add
+-- category_group
 ALTER TABLE `category_group` ADD INDEX idx1(`group_id`);
-
