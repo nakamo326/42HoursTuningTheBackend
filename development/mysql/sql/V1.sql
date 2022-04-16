d50fcd0e941e1e88c59058bdf3f128f3b2cac205
mahiro <mahiro.yoshie@gmail.com>
2022-04-17 05:00:44 +0900

    index 最初の修正に戻す

--- development/mysql/sql/V1.sql
+++ development/mysql/sql/V1.sql
@@ -1,19 +1,20 @@
 -- sqlのindexを張る作業をする予定です。
-
 -- record
 
+-- add
 ALTER TABLE `record` ADD INDEX idx1(`category_id`, `application_group`);
+
 ALTER TABLE `record` ADD INDEX idx2(`status`, `updated_at` desc, `record_id` asc);
 ALTER TABLE `record` ADD INDEX idx3(`created_by`, `status`, `updated_at` desc, `record_id` asc);
--- ALTER TABLE `record` ADD INDEX idx2(`status`, `updated_at` desc, `record_id` asc);
--- ALTER TABLE `record` ADD INDEX idx3(`created_by`, `status`, `updated_at` desc, `record_id` asc);
 
 -- record_item_file
 ALTER TABLE `record_item_file` ADD INDEX idx1(`linked_record_id`, `item_id` asc);
-
 -- session 
 ALTER TABLE `session` ADD INDEX idx1(`value`);
 
--- record_comment
--- ALTER TABLE `record_comment` ADD INDEX idx1(`linked_record_id`);
-ALTER TABLE `record_comment` ADD INDEX idx1(`created_at`);
+ALTER TABLE `record_comment` ADD INDEX idx1(`linked_record_id`);
+
+
+
+
+
--- development/mysql/sql/V2.sql
+++ development/mysql/sql/V2.sql
@@ -1,6 +1,8 @@
--- group_member
 ALTER TABLE `group_member` ADD INDEX idx1(`user_id`, `is_primary`);
+
+-- add
 ALTER TABLE `group_member` ADD INDEX idx2(`group_id`);
 
--- category_group
+-- add
 ALTER TABLE `category_group` ADD INDEX idx1(`group_id`);
+
