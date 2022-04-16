0812dd694e9bb929ef81edeb123a327ec4f047c7
mahiro <mahiro.yoshie@gmail.com>
2022-04-17 04:38:13 +0900

    index修正

--- development/mysql/sql/V2.sql
+++ development/mysql/sql/V2.sql
@@ -1,8 +1,6 @@
+-- group_member
+ALTER TABLE `group_member` ADD INDEX idx1(`group_id`);
+-- ALTER TABLE `group_member` ADD INDEX idx1(`user_id`, `is_primary`);
 
-ALTER TABLE `group_member` ADD INDEX idx1(`user_id`, `is_primary`);
-
--- add
-ALTER TABLE `group_member` ADD INDEX idx2(`group_id`);
-
--- add
+-- category_group
 ALTER TABLE `category_group` ADD INDEX idx1(`group_id`);
