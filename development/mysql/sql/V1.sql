739a5c3a7a230ffc89b71852c363d5b89ccececb
Yasufumi Nakamoto <nakamo326@gmail.com>
2022-04-16 15:51:50 +0900

    fix sql

--- development/mysql/sql/V1.sql
+++ development/mysql/sql/V1.sql
@@ -1,6 +1,6 @@
 -- sqlのindexを張る作業をする予定です。
 -- record
-ALTER TABLE `record` ADD INDEX idx1(`updated_at` desc, `record_id` asc);
+ALTER TABLE `record` ADD INDEX idx1(`status`);
 -- record_item_file
 ALTER TABLE `record_item_file` ADD INDEX idx1(`linked_record_id`, `item_id` asc);
 -- session 
