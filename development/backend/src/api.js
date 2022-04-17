5e4cee0a49636bc5334e981978438122a55e579d
Yasufumi Nakamoto <nakamo326@gmail.com>
2022-04-17 10:42:27 +0900

    conecct1000

--- development/backend/src/api.js
+++ development/backend/src/api.js
@@ -18,7 +18,7 @@ const mysqlOption = {
   password: 'backend',
   database: 'app',
   waitForConnections: true,
-  connectionLimit: 200,
+  connectionLimit: 1000,
 };
 const pool = mysql.createPool(mysqlOption);
 
