c41e48ad482746dffdfd3a86f7eaf622dc4c3ade
Yasufumi Nakamoto <nakamo326@gmail.com>
2022-04-17 13:31:38 +0900

    last commit

--- development/backend/src/api.js
+++ development/backend/src/api.js
@@ -620,8 +620,8 @@ const postFiles = async (req, res) => {
   const binary = Buffer.from(base64Data, 'base64');
 
   const image = await sharp(binary)
-    .png({ palette: true, quality: 90, force: false })
-    .jpeg({ quality: 90, force: false });
+    .png({ palette: true, quality: 80, force: false })
+    .jpeg({ quality: 80, force: false });
   const metadata = await image.metadata();
   await image.toFile(`${filePath}${newId}_${name}`, (err, info) => {
     if (info.size < 1024) {
