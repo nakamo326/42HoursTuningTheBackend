ec6ae437de391a4fe1e29048c683dda6074e8754
Yasufumi Nakamoto <nakamo326@gmail.com>
2022-04-17 11:05:26 +0900

    fix index

--- development/mysql/sql/V2.sql
+++ development/mysql/sql/V2.sql
@@ -1,9 +1,9 @@
 ALTER TABLE `group_member` ADD INDEX idx1(`user_id`, `is_primary`);
 
--- group_member
-ALTER TABLE `group_member` ADD INDEX idx2(`group_id`);
+-- -- group_member
+-- ALTER TABLE `group_member` ADD INDEX idx2(`group_id`);
 
--- category_group
-ALTER TABLE `category_group` ADD INDEX idx1(`group_id`);
+-- -- category_group
+-- ALTER TABLE `category_group` ADD INDEX idx1(`group_id`);
 
-ALTER TABLE `record` ADD INDEX idx4(`updated_at` desc, `record_id` asc);
+-- ALTER TABLE `record` ADD INDEX idx4(`updated_at` desc, `record_id` asc);
