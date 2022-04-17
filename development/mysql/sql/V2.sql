fa92df8b4b0f5d59f1b29ffe55757ff404264c44
Yasufumi Nakamoto <nakamo326@gmail.com>
2022-04-17 09:57:39 +0900

    add index

--- development/mysql/sql/V2.sql
+++ development/mysql/sql/V2.sql
@@ -5,3 +5,5 @@ ALTER TABLE `group_member` ADD INDEX idx2(`group_id`);
 
 -- category_group
 ALTER TABLE `category_group` ADD INDEX idx1(`group_id`);
+
+ALTER TABLE `record` ADD INDEX idx4(`updated_at` desc, `record_id` asc);
